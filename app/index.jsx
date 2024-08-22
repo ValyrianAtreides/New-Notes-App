d8f5150cf043b24b1d78deb29d6fa06765cdd582
Berkay Aydın <99977526+ValyrianAtreides@users.noreply.github.com>
2024-08-22 12:24:06 +0300

    gradlew configurations handled

--- app/index.jsx
+++ app/index.jsx
@@ -1,12 +1,24 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
+import { Link } from 'expo-router'
 
 const index = () => {
   return (
-    <View>
-      <Text>index</Text>
+    <View style={styles.mainContainer}>
+      <Text>merhaba</Text>
+      <Link href="/home">Go to home page</Link>
+
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    justifyContent:'center',
+    flex:1
+    
+  }
+})
+
 
 export default index
